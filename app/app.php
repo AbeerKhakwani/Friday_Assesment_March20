fad5bddb38a20d8d781cbc773ec9592baa8e6c5a
Abeer Khakwani <theKfamily10@gmail.com>
2015-03-20 16:12:31 -0700

    get Home page and style page up and running

--- app/app.php
+++ app/app.php
@@ -22,11 +22,35 @@
 
           return $app['twig']->render('home.twig', array('stylists'=>Stylist::getAll()));
       });
-   $app->post("/", function() use ($app) {
 
-        return $app['twig']->render('home.twig',array('stylists'=>Stylist::getAll()));
+
+   $app->post("/", function() use ($app) {
+       $name=$_POST['name'];
+        $new_stylist= new Stylist ( $name) ;
+        $new_stylist->save();
+     return $app['twig']->render('home.twig',array('stylists'=>Stylist::getAll()));
     });
 
+
+
+    $app->get("/stylist/{id}", function($id) use ($app) {
+        $stylist=Stylist::find($id);
+
+          return $app['twig']->render('stylist.twig', array("stylist"=>$stylist,"client"=>$stylist->getClients() ));
+      });
+
+
+   $app->post("/stylist/{id}", function($id) use ($app) {
+       $stylist_id=Stylist::find($id);
+
+
+       $name=$_POST['name'];
+       $stylist=$_POST['stylist'];
+       $new_client=new Client($name,$id, $stylist);
+       $new_client->save();
+
+     return $app['twig']->render('stylist.twig',array("stylist"=>$stylist_id,"clients"=>$stylist_id->getClients()));
+    });
 
 
 
