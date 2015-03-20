905a9e065070e46abc9ecfb0abedf7aa37e2afc7
Abeer Khakwani <theKfamily10@gmail.com>
2015-03-20 09:27:11 -0700

    Add get and set methods for stylist and test them

---
+++ tests/StylistTest.php
@@ -0,0 +1,60 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+    require_once "src/Stylist.php";
+    require_once "src/Client.php";
+    $DB = new PDO('pgsql:host=localhost;dbname=hair_salon_test');
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_getName()
+        {
+            //Arrange
+            $name = "Lynda";
+            $id = null;
+            $test_Stylist = new Stylist($name,$id);
+            //Act
+            $result = $test_Stylist->getName();
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+        function test_setType()
+         {
+             //Arrange
+             $name = "Lynda";
+             $id = null;
+             $test_Stylist = new Stylist($name,$id);
+
+             $test_Stylist->setName("Julie");
+             //Assert
+             $result = $test_Stylist->getName();
+             $this->assertEquals("Julie", $result);
+         }
+         function test_getId()
+         {
+             //Arrange
+             $name = "Lynda";
+             $id = 2;
+             $test_Stylist = new Stylist($name,$id);
+             //Act
+             $result = $test_Stylist->getId();
+             //Assert
+             $this->assertEquals(2, $result);
+         }
+         function test_setId()
+         {
+             //Arrange
+             $name = "Lynda";
+             $id = null;
+             $test_Stylist = new Stylist($name,$id);
+             //Act
+             $test_Stylist->setId(1);
+             //Assert
+             $result = $test_Stylist->getId();
+             $this->assertEquals(1, $result);
+         }
+    }
+?>
